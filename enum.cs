a4b923f17365b0202172194362aabd9c488c768e
iamnana-tyc <nanatyc7@gmail.com>
2024-06-29 11:05:35 +0500

    implemented enum

---
+++ enum.cs
@@ -0,0 +1,10 @@
+// Fighter: class (type of the fighter: front-line, interceptor, deck-based, multifunctional, tactical).
+public enum FighterType
+{
+    FrontLine,
+    Interceptor,
+    DeckBased,
+    Multifuctional,
+    Tactical
+
+}
