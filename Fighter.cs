1ac0fa80c1c58f9b9353511582e657bbdc8d29d8
iamnana-tyc <nanatyc7@gmail.com>
2024-07-01 15:01:45 +0500

    Fighter inherit Aicraft

--- Fighter.cs
+++ Fighter.cs
@@ -1,19 +1,30 @@
 
 public class Fighter : Aircraft
 {
-
+    
     public FighterType Type { get; set; }
 
-    public Fighter(string model, int yearOfProduction, int numberOfCrewMemembers, int fuelLevel, FighterType type)
-    : base(model, yearOfProduction, numberOfCrewMemembers, fuelLevel)
+   public Fighter(string model, int yearOfProduction, int numberOfCrewMemembers, FighterType type)
+    : base(model, yearOfProduction, numberOfCrewMemembers)
     {
         this.Type = type;
-
     }
+
+    public Fighter(){}
 
     public override void MaxFuel()
     {
-        FuelLevel = 1000;
+        CurrentFuelLevel = 1000;
+    }
+
+    public override void RandomInit()
+    {
+        base.RandomInit();
+        Random random = new Random();
+
+        Array fighterTypes = Enum.GetValues(typeof(FighterType));
+        Type = (FighterType)fighterTypes.GetValue(random.Next(fighterTypes.Length));
+
     }
 
     public override string ToString()
