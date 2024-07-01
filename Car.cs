7c408a256ab8f8c87764f6d77c71de7bb93672ff
iamnana-tyc <nanatyc7@gmail.com>
2024-07-01 15:00:54 +0500

    Car inherit Machine base class

--- Car.cs
+++ Car.cs
@@ -1,53 +1,54 @@
 // Car: brand, year of production, color, cost, fuel level;
 using System.Data.Common;
 
-public class Car : IDriveable, IFuelable, IRandomInit
+public class Car : Machine
 {
 
     public string Brand { get; set; }
-    public DateTime YearOfProduction { get; set; }
+    public string YearOfProduction { get; set; }
 
     public string Color { get; set; }
 
     public int Cost { get; set; }
 
-    public double FuelLevel { get; set; }
+    public Car()
+    {
 
-    public Car(string brand, string color, int cost, double fuelLevel)
+    }
+
+    public Car(string brand, string yearOfProduction, string color, int cost)
     {
         this.Brand = brand;
         this.Color = color;
+        this.YearOfProduction = yearOfProduction;
         this.Cost = cost;
-        this.FuelLevel = fuelLevel;
-
-    }
-    public double FuelConsumption { get; set; }
-    public double CurrentFuelLevel { get; set; }
-
-    public void Drive(int kilometers)
-    {
-        double fuelNeeded = kilometers * FuelConsumption;
-        if (fuelNeeded >= FuelConsumption)
-        {
-            FuelLevel = FuelLevel - FuelConsumption;
-        }
-        Console.WriteLine("You don't have enough fuel to drive");
     }
 
-    public virtual void MaxFuel()
+    public override void MaxFuel()
     {
+        CurrentFuelLevel = 100;
+    }
+
+    public override void RandomInit()
+    {
+        // This base.RandomInit() is used to initialize base class properties
+        base.RandomInit();
+
+        // now we randomly initialise the props of the Car class
+        Random random = new Random();
+        string[] brands = { "Toyota", "Skoda", "Ford", "BMW", "Audi" };
+        string[] colors = { "Red", "Blue", "Green", "Black", "White" };
+
+        Brand = brands[random.Next(brands.Length)];
+        Color = colors[random.Next(colors.Length)];
+        YearOfProduction = random.Next(1999, 2024).ToString();
+        Cost = random.Next(1000, 200000);
+
 
     }
 
     public override string ToString()
     {
-        return $"Brand: {Brand}, YearOfProduction: {YearOfProduction}, Color: {Color}, Cost: {Cost}$, FuelLevel: {FuelLevel}";
-    }
-
-    public void RandomInit()
-    {
-        Random random = new Random();
-        Brand = random.GetType().
-        throw new NotImplementedException();
+        return $"Brand: {Brand}, YearOfProduction: {YearOfProduction}, Color: {Color}, Cost: {Cost}$ ";
     }
 }
