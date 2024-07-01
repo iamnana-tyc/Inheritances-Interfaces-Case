a876e233cfa8ef7c756b8ccc027207652b115810
iamnana-tyc <nanatyc7@gmail.com>
2024-07-01 15:00:25 +0500

    Airplane inherit Aircraft

--- AirPlane.cs
+++ AirPlane.cs
@@ -1,20 +1,33 @@
 // Airplane: number of passengers, maximum flight range; 
 public class AirPlane : Aircraft
 {
-
+    
     public int NumberOfPassengers { get; set; }
     public int MaximumFlightRange { get; set; }
 
-    public AirPlane(string model, int yearOfProduction, int numberOfCrewMemembers, int fuelLevel, int numberOfPassengers, int maximumFlightRange)
-    : base(model, yearOfProduction, numberOfCrewMemembers, fuelLevel)
+    public AirPlane(){}
+
+    public AirPlane(string model, int yearOfProduction, int numberOfCrewMemembers, int numberOfPassengers, int maximumFlightRange)
+     : base(model, yearOfProduction, numberOfCrewMemembers)
     {
-        this.NumberOfPassengers = numberOfPassengers;
+        this.NumberOfPassengers =numberOfPassengers;
         this.MaximumFlightRange = maximumFlightRange;
+    }
+
+
+    public override void RandomInit()
+    {
+        base.RandomInit();
+
+        Random random = new Random();
+
+        NumberOfPassengers = random.Next(1, 1000);
+        MaximumFlightRange = random.Next(1000, 15000);
     }
 
     public override string ToString()
     {
-        return base.ToString() + $", NumberOfPassengers: {NumberOfPassengers}, MaximumFlightRange: {MaximumFlightRange} ";
+        return base.ToString() + $", NumberOfPassengers: {NumberOfPassengers}, MaximumFlightRange: {MaximumFlightRange} Km ";
     }
 
 }
